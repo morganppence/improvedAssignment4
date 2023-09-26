a2a693e5c3ea6ecf5408ab6054b5da2cef276748
morganppence <morgan.pence@colorado.edu>
2023-09-26 13:14:45 -0600

    Initial Commit

---
+++ improvedAssignment4/improvedAssignment4App.swift
@@ -0,0 +1,17 @@
+//
+//  improvedAssignment4App.swift
+//  improvedAssignment4
+//
+//  Created by Morgan Pence on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct improvedAssignment4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
